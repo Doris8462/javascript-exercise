9f545d17fde7e4409a1c3b4bd3f01d6362032a82
Doris8462 <liandou123>
2020-08-10 19:39:48 +0800

    [huangyunjie] feat:3-object

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key of Object.keys(source)) sum += Number(source[key]);
+  return sum;
 }

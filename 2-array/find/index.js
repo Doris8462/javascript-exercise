e06d9394f76e9daf73b430b72c003bb2dc900c12
Doris8462 <liandou123>
2020-08-10 18:21:13 +0800

    [huangyunjie] feat:2-array

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,9 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const myDate = new Date();
+  const thisYear = myDate.getFullYear();
+  const result = collection.find(item => {
+    return item.age <= thisYear - 2000;
+  });
+  return result.name;
 }
